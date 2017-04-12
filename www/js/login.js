c22bbac5485e03dca85e2b366f0b20c6079dae05
Vedanth <vedanth.kr@unilever.com>
2017-04-12 19:47:41 +0530

    Revamp

--- www/js/login.js
+++ www/js/login.js
@@ -1,206 +1,33 @@
 var Login = function () {
     
     return {
-        //main function to initiate the module
-        init: function () {
-        	
-           $('.login-form').validate({
-	            errorElement: 'label', //default input error message container
-	            errorClass: 'help-inline', // default input error message class
-	            focusInvalid: false, // do not focus the last invalid input
-	            rules: {
-	                username: {
-	                    required: true
-	                },
-	                password: {
-	                    required: true
-	                },
-	                remember: {
-	                    required: false
-	                }
-	            },
+        validate: function() {
 
-	            messages: {
-	                username: {
-	                    required: "Username is required."
-	                },
-	                password: {
-	                    required: "Password is required."
-	                }
-	            },
-
-	            invalidHandler: function (event, validator) { //display error alert on form submit   
-	                $('.alert-error', $('.login-form')).show();
-	            },
-
-	            highlight: function (element) { // hightlight error inputs
-	                $(element)
-	                    .closest('.control-group').addClass('error'); // set error class to the control group
-	            },
-
-	            success: function (label) {
-	                label.closest('.control-group').removeClass('error');
-	                label.remove();
-	            },
-
-	            errorPlacement: function (error, element) {
-	                error.addClass('help-small no-left-padding').insertAfter(element.closest('.input-icon'));
-	            },
-
-	            submitHandler: function (form) {
-
-	            	login();
-                }
-	        });
-
-           function login(){
-                console.log($('#username').val());
-                localStorage.setItem('username', $('input.username').val());
-                localStorage.setItem('password', $('input.password').val());
-                window.location.replace("dashboard.html");
-
+            console.log($('input.username').val());
+            if($('input.username').val() == undefined || $('input.username').val().length <= 0){
+                $('.error_message_text').text('* Username is required');
+                return false;
             }
 
-	        $('.login-form input').keypress(function (e) {
-	            if (e.which == 13) {
-	                if ($('.login-form').validate().form()) {
-                        login();
-                    }
-	                return false;
-	            }
-	        });
+            if($('input.password').val() == undefined || $('input.password').val().length <= 0){
+                $('.error_message_text').text('* Password is required');
+                return false;
+            }
 
-	        $('.forget-form').validate({
-	            errorElement: 'label', //default input error message container
-	            errorClass: 'help-inline', // default input error message class
-	            focusInvalid: false, // do not focus the last invalid input
-	            ignore: "",
-	            rules: {
-	                email: {
-	                    required: true,
-	                    email: true
-	                }
-	            },
-
-	            messages: {
-	                email: {
-	                    required: "Email is required."
-	                }
-	            },
-
-	            invalidHandler: function (event, validator) { //display error alert on form submit   
-
-	            },
-
-	            highlight: function (element) { // hightlight error inputs
-	                $(element)
-	                    .closest('.control-group').addClass('error'); // set error class to the control group
-	            },
-
-	            success: function (label) {
-	                label.closest('.control-group').removeClass('error');
-	                label.remove();
-	            },
-
-	            errorPlacement: function (error, element) {
-	                error.addClass('help-small no-left-padding').insertAfter(element.closest('.input-icon'));
-	            },
-
-	            submitHandler: function (form)
-                {
-                    login();
-
-	            }
-	        });
-
-	        $('.forget-form input').keypress(function (e) {
-	            if (e.which == 13) {
-	                if ($('.forget-form').validate().form()) {
-                        login();
-                    }
-	                return false;
-	            }
-	        });
-
-	        jQuery('#forget-password').click(function () {
-	            jQuery('.login-form').hide();
-	            jQuery('.forget-form').show();
-	        });
-
-	        jQuery('#back-btn').click(function () {
-	            jQuery('.login-form').show();
-	            jQuery('.forget-form').hide();
-	        });
-
-	        $('.register-form').validate({
-	            errorElement: 'label', //default input error message container
-	            errorClass: 'help-inline', // default input error message class
-	            focusInvalid: false, // do not focus the last invalid input
-	            ignore: "",
-	            rules: {
-	                username: {
-	                    required: true
-	                },
-	                password: {
-	                    required: true
-	                },
-	                rpassword: {
-	                    equalTo: "#register_password"
-	                },
-	                email: {
-	                    required: true,
-	                    email: true
-	                },
-	                tnc: {
-	                    required: true
-	                }
-	            },
-
-	            messages: { // custom messages for radio buttons and checkboxes
-	                tnc: {
-	                    required: "Please accept TNC first."
-	                }
-	            },
-
-	            invalidHandler: function (event, validator) { //display error alert on form submit   
-
-	            },
-
-	            highlight: function (element) { // hightlight error inputs
-	                $(element)
-	                    .closest('.control-group').addClass('error'); // set error class to the control group
-	            },
-
-	            success: function (label) {
-	                label.closest('.control-group').removeClass('error');
-	                label.remove();
-	            },
-
-	            errorPlacement: function (error, element) {
-	                if (element.attr("name") == "tnc") { // insert checkbox errors after the container                  
-	                    error.addClass('help-small no-left-padding').insertAfter($('#register_tnc_error'));
-	                } else {
-	                    error.addClass('help-small no-left-padding').insertAfter(element.closest('.input-icon'));
-	                }
-	            },
-
-	            submitHandler: function (form) {
-                    login();
-
-                }
-	        });
-
-	        jQuery('#register-btn').click(function () {
-	            jQuery('.login-form').hide();
-	            jQuery('.register-form').show();
-	        });
-
-	        jQuery('#register-back-btn').click(function () {
-	            jQuery('.login-form').show();
-	            jQuery('.register-form').hide();
-	        });
+            return true;
+        },
+        login: function () {
+            if(this.validate()){
+                localStorage.setItem('username', $('input.username').val());
+                localStorage.setItem('password', $('input.password').val());
+                window.location.replace('landing.html');
+            }
+        },
+        init: function() {
+            $('button').on('click',function () {
+                Login.login();
+            });
         }
-
     };
 
 }();
